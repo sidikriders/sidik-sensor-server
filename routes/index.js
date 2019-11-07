c9e8b526f14b850584da72313e0fb7c480e10982
sidikriders <sidik@xwork.id>
2019-11-07 20:13:51 +0700

    add sensor route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const area = require('./area')
+const sensor = require('./sensor')
 
 router.use('/area', area)
+router.use('/sensor', sensor)
 
 module.exports = router
